86d7c258b92931b416c1c99dbaca3573a09afcce
KVH <h.kurd@vasl.ir>
2021-05-28 23:15:07 +0430

    add module

--- app/src/main/java/com/hosseinkurd/smacksample/MainActivity.kt
+++ app/src/main/java/com/hosseinkurd/smacksample/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.hosseinkurd.smacksample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import org.jivesoftware.smack.AbstractXMPPConnection
+import org.jivesoftware.smack.tcp.XMPPTCPConnection
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Create a connection and login to the example.org XMPP service.
+        // Create a connection and login to the example.org XMPP service.
+        val conn1: AbstractXMPPConnection = XMPPTCPConnection("username", "password", "example.org")
+        conn1.connect().login()
     }
 }
